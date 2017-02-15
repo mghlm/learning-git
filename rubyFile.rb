7546124bea4ebbf42ace59ea68ad6dd00b7db9ef
Magnus Holm <mail@magnusholm.com>
2017-02-15 12:06:23 +0000

    added an extra argument to  multiply method

--- rubyFile.rb
+++ rubyFile.rb
@@ -1,11 +1,11 @@
 def add(a, b, c)
-  return a + b + c 
+  return a + b + c
 end
 
 def substract(a, b)
   return a - b
 end
 
-def multiply(a, b)
-  return a * b
+def multiply(a, b, c)
+  return (a * b) * c 
 end
